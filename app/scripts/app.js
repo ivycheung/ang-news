36a82f265c739c2b70536f58ba917356e82517b8
Ivy Cheung <i.cheung@sap.com>
2014-05-20 16:47:53 -0700

    init
    Change-Id: I0308d179043fc942ef8e7cf93eb2b1197b8a5803

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+/* global app:true */
+'use strict';
+
+var app = angular
+  .module('angNewsApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'firebase'
+  ]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+            templateUrl: 'views/showpost.html',
+            controller: 'PostViewCtrl'
+        })
+      .otherwise({
+        redirectTo: '/'
+      });
+}).
+  constant('FIREBASE_URL', 'https://incandescent-fire-3215.firebaseio.com/');
