ba00ab3d87ee3aabae429bca77d00b638f576cb6
Ivy Cheung <i.cheung@sap.com>
2014-05-21 16:54:42 -0700

    Completed tutorial
    Change-Id: I6f6a04165820e02d783cc952b7fa15a143ed032f

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,26 +2,38 @@
 'use strict';
 
 var app = angular
-  .module('angNewsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'firebase'
-  ]);
+    .module('angNewsApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'firebase'
+    ]);
 
 app.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/posts.html',
-        controller: 'PostsCtrl'
-      })
-      .when('/posts/:postId', {
+        .when('/', {
+            templateUrl: 'views/posts.html',
+            controller: 'PostsCtrl'
+        })
+        .when('/posts/:postId', {
             templateUrl: 'views/showpost.html',
             controller: 'PostViewCtrl'
         })
-      .otherwise({
-        redirectTo: '/'
-      });
+        .when('/register', {
+            templateUrl: 'views/register.html',
+            controller: 'AuthCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'AuthCtrl'
+        })
+        .when('/users/:username', {
+            templateUrl: 'views/profile.html',
+            controller: 'ProfileCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }).
-  constant('FIREBASE_URL', 'https://incandescent-fire-3215.firebaseio.com/');
+    constant('FIREBASE_URL', 'https://incandescent-fire-3215.firebaseio.com/');
